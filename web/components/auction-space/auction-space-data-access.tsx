b66868b25c22886ae8b133a16880a303b34ee317
Sam Devorsetz <samdevorsetz@gmail.com>
2024-02-24 19:28:04 -0500

    adding !!pubKey check

--- web/components/auction-space/auction-space-data-access.tsx
+++ web/components/auction-space/auction-space-data-access.tsx
@@ -2,11 +2,10 @@
 
 import { AuctionSpaceIDL, getAuctionSpaceProgramId, AuctionSpaceSDK } from '@auction-app/solana';
 import { Program } from '@coral-xyz/anchor';
-import { WalletContextState, useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { Cluster, Keypair, PublicKey } from '@solana/web3.js';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { Cluster, PublicKey } from '@solana/web3.js';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
-import toast from 'react-hot-toast';
 import { useCluster } from '../cluster/cluster-data-access';
 import { useAnchorProvider } from '../solana/solana-provider';
 import { useTransactionToast } from '../ui/ui-layout';
@@ -64,38 +63,36 @@ export function useAuctionSpaceInitMethods() {
   const { connection } = useConnection();
   const transactionToast = useTransactionToast();
 
-  if (!wallet.publicKey) {
-    throw new Error('Wallet required for useAuctionSpaceWithWallet');
-  }
-
   const sdk = new AuctionSpaceSDK(connection, program, wallet);
 
   const publisher = useQuery({
     queryKey: ['publisher', 'fetch', wallet.publicKey],
     queryFn: () => sdk.getPublisher(),
+    enabled: !!wallet.publicKey,
   });
 
   const newPublisher = useMutation({
     mutationKey: ['publisher', 'create', wallet.publicKey],
     mutationFn: () => sdk.newPublisher(),
-    onSuccess: (tx) => {
+    onSuccess: wallet.publicKey ? (tx) => {
       transactionToast(tx);
       return publisher.refetch();
-    },
+    } : undefined,
   });
 
   const advertiser = useQuery({
     queryKey: ['advertiser', 'fetch', wallet.publicKey],
     queryFn: () => sdk.getAdvertiser(),
+    enabled: !!wallet.publicKey,
   });
 
   const newAdvertiser = useMutation({
     mutationKey: ['advertiser', 'create', wallet.publicKey],
     mutationFn: () => sdk.newAdvertiser(),
-    onSuccess: (tx) => {
+    onSuccess: wallet.publicKey ? (tx) => {
       transactionToast(tx);
       return advertiser.refetch();
-    },
+    } : undefined,
   });
 
   return {
@@ -106,59 +103,59 @@ export function useAuctionSpaceInitMethods() {
   };
 }
 
-export function useAuctionSpaceProgramAccount({ counter }: { counter: PublicKey }) {
-  const { cluster } = useCluster();
-  const transactionToast = useTransactionToast();
-  const { program, accounts } = useAuctionSpaceProgram();
+// export function useAuctionSpaceProgramAccount({ counter }: { counter: PublicKey }) {
+//   const { cluster } = useCluster();
+//   const transactionToast = useTransactionToast();
+//   const { program, accounts } = useAuctionSpaceProgram();
 
-  const account = useQuery({
-    queryKey: ['counter', 'fetch', { cluster, counter }],
-    queryFn: () => program.account.counter.fetch(counter),
-  });
+//   const account = useQuery({
+//     queryKey: ['counter', 'fetch', { cluster, counter }],
+//     queryFn: () => program.account.counter.fetch(counter),
+//   });
 
-  const close = useMutation({
-    mutationKey: ['counter', 'close', { cluster, counter }],
-    mutationFn: () =>
-      program.methods.closeAuctionSpace().accounts({ counter }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx);
-      return accounts.refetch();
-    },
-  });
+//   const close = useMutation({
+//     mutationKey: ['counter', 'close', { cluster, counter }],
+//     mutationFn: () =>
+//       program.methods.closeAuctionSpace().accounts({ counter }).rpc(),
+//     onSuccess: (tx) => {
+//       transactionToast(tx);
+//       return accounts.refetch();
+//     },
+//   });
 
-  const decrement = useMutation({
-    mutationKey: ['counter', 'decrement', { cluster, counter }],
-    mutationFn: () => program.methods.decrement().accounts({ counter }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx);
-      return account.refetch();
-    },
-  });
+//   const decrement = useMutation({
+//     mutationKey: ['counter', 'decrement', { cluster, counter }],
+//     mutationFn: () => program.methods.decrement().accounts({ counter }).rpc(),
+//     onSuccess: (tx) => {
+//       transactionToast(tx);
+//       return account.refetch();
+//     },
+//   });
 
-  const increment = useMutation({
-    mutationKey: ['counter', 'increment', { cluster, counter }],
-    mutationFn: () => program.methods.increment().accounts({ counter }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx);
-      return account.refetch();
-    },
-  });
+//   const increment = useMutation({
+//     mutationKey: ['counter', 'increment', { cluster, counter }],
+//     mutationFn: () => program.methods.increment().accounts({ counter }).rpc(),
+//     onSuccess: (tx) => {
+//       transactionToast(tx);
+//       return account.refetch();
+//     },
+//   });
 
-  const set = useMutation({
-    mutationKey: ['counter', 'set', { cluster, counter }],
-    mutationFn: (value: number) =>
-      program.methods.set(value).accounts({ counter }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx);
-      return account.refetch();
-    },
-  });
+//   const set = useMutation({
+//     mutationKey: ['counter', 'set', { cluster, counter }],
+//     mutationFn: (value: number) =>
+//       program.methods.set(value).accounts({ counter }).rpc(),
+//     onSuccess: (tx) => {
+//       transactionToast(tx);
+//       return account.refetch();
+//     },
+//   });
 
-  return {
-    account,
-    close,
-    decrement,
-    increment,
-    set,
-  };
-}
+//   return {
+//     account,
+//     close,
+//     decrement,
+//     increment,
+//     set,
+//   };
+// }
